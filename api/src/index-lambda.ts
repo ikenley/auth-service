5dd690de6b70ff782fd31f51efc17545edb519aa
ikenley <ikenley6@gmail.com>
2023-11-05 07:52:34 -0500

    SsmParamLoader

--- api/src/index-lambda.ts
+++ api/src/index-lambda.ts
@@ -3,15 +3,23 @@ import "express-async-errors";
 import { container } from "tsyringe";
 import serverlessExpress from "@vendia/serverless-express";
 import { ALBEvent, Context } from "aws-lambda";
+import { SSMClient } from "@aws-sdk/client-ssm";
 import { getConfigOptions } from "./config";
 import express from "express";
 import Logger from "./loaders/logger";
+import SsmParamLoader from "./loaders/SsmParamLoader";
 import loadGlobalDependencies from "./loaders/loadGlobalDependencies";
 import ExpressLoader from "./loaders/ExpressLoader";
 
 let serverlessExpressInstance: any = null;
 
 const setup = async (event: ALBEvent, context: Context) => {
+  // Inject SSM param configuration into env vars
+  const ssmClient = new SSMClient();
+  const ssmParamLoader = new SsmParamLoader(ssmClient);
+  const configParamName = process.env.CONFIG_SSM_PARAM_NAME!;
+  await ssmParamLoader.loadToEnv(configParamName);
+
   const config = getConfigOptions();
   const app = express();
 
